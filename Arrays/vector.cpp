893cf03f851b72b9284acf6870fbceeb0c643f52
Eshwari Khurd <eshwarikhurd6@gmail.com>
2024-08-05 14:10:26 +0530

    commit

---
+++ Arrays/vector.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int main(){
+
+    vector<int> v;
+    v.push_back(5);
+    v.push_back(2);
+    v.push_back(3);
+
+    
+
+    vector<int>::iterator it;
+    for(it=v.begin();it!=v.end();it++){
+        cout<<*it;
+    }
+    cout<<endl;
+    for(auto element:v){
+        cout<<element<<endl;
+    }
+    v.pop_back();
+    cout<<endl;
+    for(auto element:v){
+        cout<<element<<endl;
+    }
+
+    
+    return 0;
+
+}
